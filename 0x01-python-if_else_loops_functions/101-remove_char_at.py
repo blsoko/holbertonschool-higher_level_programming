f41af390365c5cde42e76283f4a99013b65ed49a
blsoko <exanymejose1@hotmail.com>
2020-04-29 03:33:49 +0000

    advanced exercices

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+        if n < 0:
+                str2 = str[0:n] + str[n:]
+        else:
+                str2 = str[0:n] + str[n + 1:]
+        return(str2)
