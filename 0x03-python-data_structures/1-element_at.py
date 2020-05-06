5f702e4a683cfd654e84640b5b0e23853f4ce871
blsoko <exanymejose1@hotmail.com>
2020-05-06 02:18:03 +0000

    checker issues

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx >= 0 and idx <= len(my_list)):
-        return(my_list[idx])
+    if my_list:
+        if (idx >= 0 and idx <= len(my_list)):
+            return(my_list[idx])
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    b = [] + my_list
-    if (idx >= 0 and idx <= len(b)):
-        b[idx] = element
+    if my_list:
+        b = [] + my_list
+        if (idx >= 0 and idx <= len(b)):
+            b[idx] = element
         return(b)
-    return(b)
+    return(my_list)
