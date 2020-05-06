d852902c2f3ba792b49cb56de130d924de3d5f4d
blsoko <exanymejose1@hotmail.com>
2020-05-06 02:48:16 +0000

    checker issue

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix[0])):
         for j in range(0, len(matrix[0])):
             if j != len(matrix[0]) - 1:
-                print(matrix[i][j], end=" ")
+                print("{:d}".format(matrix[i][j]), end=" ")
             else:
-                print(matrix[i][j], end="")
+                print("{:d}".format(matrix[i][j]), end="")
         print()
