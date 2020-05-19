26ef8eb68c13a21f4ddb9fcd103feea5c2a906b9
blsoko <exanymejose1@hotmail.com>
2020-05-19 03:31:00 +0000

    me quiero morirrrrr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as er:
-        print(er)
+        print("Exception:", er, file=sys.stderr)
         return False
     else:
         return True
