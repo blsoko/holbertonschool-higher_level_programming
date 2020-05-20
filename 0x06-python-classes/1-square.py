5a41eef5c97ef5da794b7c52c9def5cdbe2f88ef
blsoko <exanymejose1@hotmail.com>
2020-05-20 02:58:21 +0000

    lets check

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 class Square:
-    ''' Create an empty class Square '''
+    '''Create an empty class Square'''
     def __init__(self, size=0):
-        ''' Define Square instance in size '''
+        '''Define Square instance in size'''
         self.__size = size
