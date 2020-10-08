7561b329116d0cb7d558635a924350bb9082df43
blsoko <exanymejose1@hotmail.com>
2020-10-07 20:24:27 -0500

    Readme update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ def read_file(filename=""):
     """ Read file
 
     Args:
-        filename (str, optional): [doc]. Defaults to "read mode".
+        filename (str): [doc]. Defaults to "read mode".
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+        print(f.read())
