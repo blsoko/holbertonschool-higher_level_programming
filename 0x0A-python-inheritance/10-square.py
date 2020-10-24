85ec45b7b1787313b2d5acc370222cfff6628d2f
blsoko <exanymejose1@hotmail.com>
2020-10-24 14:32:51 -0500

    checking

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,9 +7,9 @@ class Square(Recta):
     """Write a class Square that inherits from Rectangle"""
     def __init__(self, size):
         """ initialization """
-        super().__init__(size, size)
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """area of square"""
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,9 +7,9 @@ class Square(Recta):
     """Write a class Square that inherits from Rectangle"""
     def __init__(self, size):
         """ initialization """
-        super().__init__(size, size)
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """area of square"""
@@ -17,4 +17,4 @@ class Square(Recta):
 
     def __str__(self):
         """str method"""
-        return str("[Square] {}/{}".format(self.__size, self.__size))
+        return "[Square] {}/{}".format(self.__size, self.__size)
