a018b4fd2183315a867c3510417ac0997a264383
blsoko <exanymejose1@hotmail.com>
2020-11-17 14:51:55 -0500

    cities and states

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- script that lists all the cities of California
 
-SELECT states.id, cities.name FROM states, cities WHERE states.name=(SELECT states.name FROM states WHERE name="California") and states.id=cities.state_id; 
-
+SELECT states.id, cities.name FROM states, cities WHERE states.name=(SELECT states.name FROM states WHERE name="California") and states.id=cities.state_id ORDER BY id;
