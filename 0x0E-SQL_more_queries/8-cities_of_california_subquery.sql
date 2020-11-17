427fb73d55389cc30526d9b50123cdb406cd62f9
blsoko <exanymejose1@hotmail.com>
2020-11-17 14:32:44 -0500

    join 2 tables whithout join/ add id check

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- script that lists all the cities of California
 
-SELECT states.id, cities.name FROM states, cities WHERE states.name=(SELECT states.name FROM states WHERE name="California"); 
+SELECT states.id, cities.name FROM states, cities WHERE states.name=(SELECT states.name FROM states WHERE name="California") and states.id=cities.state_id; 
+
