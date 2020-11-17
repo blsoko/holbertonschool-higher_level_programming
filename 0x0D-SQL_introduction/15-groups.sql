7c1edb77d74931d7b57627f09b8ea9b05150d497
blsoko <exanymejose1@hotmail.com>
2020-11-16 22:09:03 -0500

    setting var as number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- script that lists the number of records
-SELECT score, COUNT(score) FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(score) as `number` FROM second_table GROUP BY score DESC;
