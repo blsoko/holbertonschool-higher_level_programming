1cc79f735b52c6042e55a1b469251e5fa528ba8d
blsoko <exanymejose1@hotmail.com>
2020-12-12 18:12:23 -0500

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     getQueryCursor = db.cursor()
-    getQueryCursor.execute("SELECT * FROM states WHERE " +
+    getQueryCursor.execute("SELECT * FROM states WHERE BINARY " +
                            "name='{}' ORDER BY id".format(sys.argv[4]))
     for state in getQueryCursor.fetchall():
         print(state)
