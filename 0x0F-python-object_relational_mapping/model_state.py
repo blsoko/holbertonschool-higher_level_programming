95eb9a85f2b3bc636191a56b5e74c4eeb4521968
blsoko <exanymejose1@hotmail.com>
2020-12-12 21:10:37 -0500

    adding a basic table schema

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+"""Write a python file that contains the class definition
+of a State and an instance Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Basic table schema"""
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
