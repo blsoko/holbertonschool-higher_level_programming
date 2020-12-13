040a8e624f2a04725c585b5e2dc10cb53534b830
blsoko <exanymejose1@hotmail.com>
2020-12-13 11:41:06 -0500

    get first element

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a script that prints the first
+State object from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import desc
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
+
+    queryState = session.query(State).filter(State.id == 1)
+    for names in queryState:
+        if queryState:
+            print("{}: {}".format(names.id, names.name))
+        else:
+            print("Nothing")
+            break
