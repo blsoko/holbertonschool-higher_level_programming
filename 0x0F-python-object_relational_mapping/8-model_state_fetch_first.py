b5aaadd5196d8db5099916db6e4d1fdfac9949a1
blsoko <exanymejose1@hotmail.com>
2020-12-13 11:44:07 -0500

    moving imports onto the main

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Write a script that prints the first
 State object from the database hbtn_0e_6_usa"""
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import desc
 
 if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import desc
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
