cdb32aa8bc084995341e4ab09e355384865d5e06
blsoko <exanymejose1@hotmail.com>
2020-12-13 11:47:24 -0500

    changing logical thinking

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """Write a script that prints the first
 State object from the database hbtn_0e_6_usa"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import desc
 
 if __name__ == '__main__':
-    import sys
-    from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import desc
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
@@ -18,10 +17,8 @@ if __name__ == '__main__':
 
     Base.metadata.create_all(engine)
 
-    queryState = session.query(State).filter(State.id == 1)
-    for names in queryState:
-        if queryState:
-            print("{}: {}".format(names.id, names.name))
-        else:
-            print("Nothing")
-            break
+    queryState = session.query(State).first()
+    if queryState:
+        print("{}: {}".format(queryState.id, queryState.name))
+    else:
+        print("Nothing")
