66b0f37a4371b7d2a2ec7e59962a8fef844e9def
blsoko <exanymejose1@hotmail.com>
2020-12-18 14:41:34 -0500

    cchanged for

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,7 +8,7 @@ def find_peak(list_of_integers):
         return
     elif len(list_of_integers) == 1:
         return list_of_integers[0]
-    for i in range(1, len(list_of_integers) - 1):
+    for i in range(0, len(list_of_integers)):
         if i == len(list_of_integers) - 1:
             if list_of_integers[i] > list_of_integers[i - 1]:
                 return list_of_integers[i]
