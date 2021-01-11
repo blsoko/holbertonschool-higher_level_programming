53ebe1d421182037edb84155ed692b8313095a35
blsoko <exanymejose1@hotmail.com>
2021-01-11 13:22:47 -0500

    advancced task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,9 +9,12 @@ if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
     r = requests.get(url)
     cont = 0
-    for i in r.json():
-        cont += 1
-        print("{}: {}".format(i.get('sha'), i.get('commit')
-              .get('author').get('name')))
-        if cont == 10:
-            break
+    try:
+        for i in r.json():
+            cont += 1
+            print("{}: {}".format(i.get('sha'), i.get('commit')
+                .get('author').get('name')))
+            if cont == 10:
+                break
+    except:
+        pass
